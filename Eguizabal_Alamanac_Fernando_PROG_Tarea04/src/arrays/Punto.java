45e002a422daf05e8e3a6997670dbd9fde19cdf2
fagui <fagui@Maria>
2017-11-17 20:02:29 +0100

    Creo el método para calcular la distancia entre este punto y otro pasado por parámetros

--- Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Punto.java
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Punto.java
@@ -29,4 +29,14 @@ public class Punto {
         String representacion = "("+this.x+","+this.y+")";
         return representacion;
     }
+    
+    //Creo el método para saber la distancia entre este punto y otro
+    public double distancia (Punto punto2){
+        double distancia = 0;
+        
+        //calculamos la distancia entre este punto y el pasado por parametros
+        distancia = Math.sqrt(Math.pow((punto2.x-this.x),2)+Math.pow((punto2.y+-this.y),2));
+        
+        return distancia;
+    }
 }
