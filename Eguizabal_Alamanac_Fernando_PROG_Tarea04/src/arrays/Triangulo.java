3a9422c7dc2fd6c8ae10556a1682a1c33e6cf053
fagui <fagui@Maria>
2017-11-17 21:08:00 +0100

    Creo el constructor del triangulo pasandole por arametros 3 puntos

--- Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Triangulo.java
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Triangulo.java
@@ -10,4 +10,12 @@ public class Triangulo {
     private Punto punto1;
     private Punto punto2;
     private Punto punto3;
+    
+    //Creo el construstor del triangulo por los parametros de los tres vertices
+    public Triangulo(Punto punto1, Punto punto2, Punto punto3){
+        //creamos los tres puntos del triangulo
+        this.punto1 = new Punto(punto1.getX(),punto1.getY());
+        this.punto2 = new Punto(punto2.getX(),punto2.getY());
+        this.punto3 = new Punto(punto3.getX(),punto3.getY());
+    }
 }
