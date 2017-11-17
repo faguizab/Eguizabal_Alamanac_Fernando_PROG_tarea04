e9cad0fb9a97ccd9a6ec3a06873772af663e4749
fagui <fagui@Maria>
2017-11-17 19:51:42 +0100

    Creo los métodos GET para los dos atributos del Punto

--- Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Punto.java
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Punto.java
@@ -10,4 +10,17 @@ public class Punto {
     private double x;
     private double y;
     
+    //Creamos el constructor del punto
+    public Punto (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    //Creamos los métodos GET para los dos atributos
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
 }
