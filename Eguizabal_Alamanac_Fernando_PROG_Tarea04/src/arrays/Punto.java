c43fd198c0b24ec1b43dd843013e32ea1afc0aaf
fagui <fagui@Maria>
2017-11-17 19:48:24 +0100

    Creo el paquete arrays con la clase Punto y sus dos atributos

---
+++ Eguizabal_Alamanac_Fernando_PROG_Tarea04/src/arrays/Punto.java
@@ -0,0 +1,13 @@
+
+package arrays;
+
+/**
+ * Clase Punto del Ejercicio 3 de la Tarea Online 4
+ * @author Fernando Eguizabal Alamañac
+ */
+public class Punto {
+    //Creamos los dos atributos como privados
+    private double x;
+    private double y;
+    
+}
